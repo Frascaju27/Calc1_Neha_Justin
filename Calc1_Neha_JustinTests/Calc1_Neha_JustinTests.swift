b6963b183b89d40c7735aba9f81ec21c66fa4552
Justin Frasca <frasca18@gmail.com>
2018-03-23 23:47:39 -0400

    Initial Commit

---
+++ Calc1_Neha_JustinTests/Calc1_Neha_JustinTests.swift
@@ -0,0 +1,36 @@
+//
+//  Calc1_Neha_JustinTests.swift
+//  Calc1_Neha_JustinTests
+//
+//  Created by Justin Frasca on 2018-03-23.
+//  Copyright © 2018 Justin Frasca. All rights reserved.
+//
+
+import XCTest
+@testable import Calc1_Neha_Justin
+
+class Calc1_Neha_JustinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
